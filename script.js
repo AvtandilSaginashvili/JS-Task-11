036b693d33fe96123aa0f305ab12a47a9da91ab9
ავთანდილ საგინაშვილი <120257059+AvtandilSaginashvili@users.noreply.github.com>
2023-05-13 12:37:57 +0400

    update

---
+++ script.js
@@ -0,0 +1,14 @@
+var x = 1;
+
+while (x < 10) {
+    x ++;
+    if (x == 1) {
+        continue;
+    }else if (x == 5) {
+        continue;
+    }else if (x == 9) {
+        continue;
+    }
+    document.write (x + "");
+
+}
